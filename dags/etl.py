8e61256bf9962d5f78cde9a63a1bc1189f638a24
Danu Pranantha <dpranantha@bol.com>
2019-02-26 13:30:47 +0100

    dataproc

--- dags/etl.py
+++ dags/etl.py
@@ -2,6 +2,12 @@ import airflow
 from airflow import DAG
 from airflow_training.operators.postgres_to_gcs import PostgresToGoogleCloudStorageOperator
 from airflow_training.operators.http_to_gcs import HttpToGcsOperator
+from airflow.contrib.operators.dataproc_operator import (
+    DataprocClusterCreateOperator,
+    DataprocClusterDeleteOperator,
+    DataProcPySparkOperator,
+)
+from airflow.utils.trigger_rule import TriggerRule
 
 dag = DAG(
     dag_id="etl_airflow",
@@ -10,7 +16,6 @@ dag = DAG(
         "start_date": airflow.utils.dates.days_ago(3),
     },
 )
-
 
 pgsl_to_gcs = PostgresToGoogleCloudStorageOperator(
     task_id="pg_to_bq",
@@ -21,14 +26,42 @@ pgsl_to_gcs = PostgresToGoogleCloudStorageOperator(
     dag=dag,
 )
 
-for currency in {'EUR', 'USD'}:
+currencies = [
     HttpToGcsOperator(
         task_id="get_currency_" + currency,
         endpoint="convert-currency?date={{ ds }}&from=GBP&to=" + currency,
         gcs_bucket="dpranantha",
         gcs_path="currency_{{ ds }}_" + currency + "_{}.json",
         http_conn_id="currency_converter",
-        dag=dag
-    )
+        dag=dag,
+    ) for currency in ['EUR', 'USD']
+]
 
+dataproc_create_cluster = DataprocClusterCreateOperator(
+    task_id="create_dataproc",
+    cluster_name="analyse-pricing-{{ ds }}",
+    project_id="Training Boldotcom - dprananth",
+    num_workers=2,
+    zone="europe-west4-a",
+    dag=dag,
+)
 
+compute_aggregates = DataProcPySparkOperator(
+    task_id='compute_aggregates',
+    main='gs://dpranantha/statistics/build_statistics.py',
+    cluster_name='analyse-pricing-{{ ds }}',
+    arguments=[
+        "gs://dpranantha/currency_{{ ds }}*.json",
+        "gs://dpranantha/land_price_uk_{{ ds }}*.json"],
+    dag=dag,
+)
+
+dataproc_delete_cluster = DataprocClusterDeleteOperator(
+    task_id="delete_dataproc",
+    cluster_name="analyse-pricing-{{ ds }}",
+    project_id="Training Boldotcom - dprananth",
+    trigger_rule=TriggerRule.ALL_DONE,
+    dag=dag,
+)
+
+[pgsl_to_gcs, currencies] >> dataproc_create_cluster >> compute_aggregates >> dataproc_delete_cluster
